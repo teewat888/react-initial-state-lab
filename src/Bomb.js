609282c0ebde19e55655443dceca198e066926ae
teerawat prarom <teewat@yahoo.com>
2021-09-10 08:08:02 +1000

    completed assignment

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,17 @@
 // your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+         secondsLeft: this.props.initialCount
+        }
+    }
+    render() {
+       // console.log('initial count: ',this.props.initialCount);
+        return <div>{this.state.secondsLeft !== 0 ? this.state.secondsLeft+' seconds left before I go boom!' : 'Boom!'}</div>;
+    }
+}
+
+export default Bomb;
